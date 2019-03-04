7b6ce76c2127b6a07dd1ac3505d23d24e7ca66cb
DavidAwgbemila <david.awogbemila@bison.howard.edu>
2019-03-04 14:29:21 -0500

    Read code string from submission in event["code"] into temporary file and run via subprocess.

--- backend/code_submission/lambda_function.py
+++ backend/code_submission/lambda_function.py
@@ -1,21 +1,22 @@
 import json
 import os
-import subprocess
+from subprocess import Popen, PIPE
 
 def lambda_handler(event, context):
-    dir_list = os.listdir("./")
-    dir_list_str = "".join(dir_list)
+    code_str = event["code"]
     
-    target_file_path_str = "/tmp/target.py"
+    target_file_path_str = "/tmp/submission.py"
     with open(target_file_path_str, "w+") as target_file:
-        with open("source.py") as source_file:
-            for line in source_file:
-                target_file.write(line)
-                
-        res = subprocess.Popen(["python3", target_file_path_str])
-        res = res.communicate()
-    
+        target_file.write(code_str)
+        
+    p = Popen(["python3", "-u", target_file_path_str], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    res =""
+    for line in p.stdout:
+        res += line.decode("utf-8")
+
     return {
         'statusCode': 200,
-        'body': json.dumps({'data': 'Hello from Lambda!' + dir_list_str, 'message':'CORS enabled', 'res': res})
+        'body': json.dumps({'res': res}),
+        'event-info': event
     }
