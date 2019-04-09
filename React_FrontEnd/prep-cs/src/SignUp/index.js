46b2019398e7b2d5783a5e0b6dd6b3bbe5301293
shrijanand <shrijanand@gmail.com>
2019-04-09 14:41:33 -0400

    Slight Update in Sign Up Page

--- React_FrontEnd/prep-cs/src/SignUp/index.js
+++ React_FrontEnd/prep-cs/src/SignUp/index.js
@@ -18,7 +18,31 @@ const INITIAL_STATE = {
 
 const SignUpPage = () => (
   <div>
-    <h1>SignUp</h1>
+    <div className="jumbotron" style={{ marginTop: "5vh", marginBottom: "5vh", marginLeft: "5vw", marginRight: "5vw"}} >
+            <h1 className="display-3">Welcome to PrepCS!</h1>
+            <p className="lead">PrepCS is web service aimed at providing Computer Science students at Howard University a personalized career development platform on 3 major fronts:</p>
+            <hr className="my-4"></hr>
+            <div className="list-group">
+              <a href="/dashboard" style={{ color: "white", backgroundColor: "#e51937" }} className="list-group-item list-group-item-action flex-column align-items-start">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">Personalized Dashboard</h5>
+                </div>
+                <p className="mb-1">Analyzes your statistics to accelerate your preparation.</p>
+              </a>
+              <a href="/courses" className="list-group-item list-group-item-action flex-column align-items-start">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">Coding Interview Readiness</h5>
+                </div>
+                <p className="mb-1">Prepares you for Technical and Behavioral Interviews.</p>
+              </a>
+              <a href="/events" className="list-group-item list-group-item-action flex-column align-items-start active">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">CS-Related Event Tracking</h5>
+                </div>
+                <p className="mb-1">Always be in the know to further your career goals.</p>
+              </a>
+            </div>
+          </div>
     <div className="auth-box">
       <SignUpForm />
     </div>
@@ -100,6 +124,8 @@ class SignUpFormBase extends Component {
       username === '';
 
     return (
+      <div>
+          <h1>SignUp</h1>
       <form onSubmit={this.onSubmit}>
         <div className="auth-box-input">
           First Name<br></br>
@@ -168,6 +194,7 @@ class SignUpFormBase extends Component {
 
         {error && <p>{error.message}</p>}
       </form>
+      </div>
     );
   }
 }
