6fef3aa9e6206ab8ad7d75cff141be633f0e282d
shrijanand <shrijanand@gmail.com>
2019-04-12 01:00:07 -0400

    Sign In Page Styling, Background Color correction

--- React_FrontEnd/prep-cs/src/SignIn/index.js
+++ React_FrontEnd/prep-cs/src/SignIn/index.js
@@ -8,10 +8,17 @@ import { SignUpLink } from '../SignUp';
 import { withFirebase } from '../Firebase';
 
 const SignInPage = () => (
-  <div>
-    <h1 style={{textAlign: 'center'}}>Sign In</h1>
+  /* Change minHeight later. Remove width styling for Mobile.*/
+  <div style={{minHeight: "900px", backgroundColor: "#002a42"}}>
+  <div className="card text-white bg-primary mb-3" style={{boxShadow: "0px 0px 10px 5px rgba(0,0,0,.3)", top: "50%", left: "50%", transform: "translate(-50%, 50%)", width: "50%"}}>
+  <div className="card-header">
+    <h3 style={{textAlign: 'center'}}>Sign In<small className="text-muted"> to PrepCS</small></h3>
+    </div>
+    <div className="card-body">
     <SignInForm />
     <SignUpLink />
+    </div>
+  </div>
   </div>
 );
 
@@ -25,6 +32,11 @@ class SignInFormBase extends Component {
   constructor(props) {
     super(props);
 
+    this.state = { data: this.props }
+    this.state = { width: '1920' };
+    this.state = { height: '1080' };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+
     this.state = { ...INITIAL_STATE };
 
     this._isMounted = false;
@@ -32,10 +44,17 @@ class SignInFormBase extends Component {
 
   componentDidMount() {
     this._isMounted = true;
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
   }
 
   componentWillUnmount() {
     this._isMounted = false;
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
   onSubmit = event => {
@@ -65,26 +84,35 @@ class SignInFormBase extends Component {
 
     return (
       <form onSubmit={this.onSubmit}>
-        <input
+        <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text">Email Address</span>
+            </div>
+        <input className="form-control"
           name="email"
           id="sign-in"
-          className="form-control"
           value={email}
           onChange={this.onChange}
           type="text"
-          placeholder="Email Address"
+          placeholder="Enter Email Address Here"
         />
-        <input
+        </div>
+        
+        <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            </div>
+        <input className="form-control"
           name="password"
           id="password"
-          className="form-control sign-in"
           value={password}
           onChange={this.onChange}
           type="password"
-          placeholder="Password"
+          placeholder="Enter Password Here"
         />
+        </div>
         <div style={{textAlign: 'center'}}>
-        <button disabled={isInvalid} type="submit" className="btn btn-primary">
+        <button disabled={isInvalid} type="submit" className="btn btn-secondary">
           Sign In
         </button>
         </div>
